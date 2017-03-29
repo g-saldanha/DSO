9b307a01c06a0bef389baade37b0f8b47c6ae284
g-saldanha <saldanha@bridge.ufsc.br>
2017-03-29 15:01:43 -0300

    Exercicio 02 - Inicio

---
+++ Animal.java
@@ -0,0 +1,12 @@
+/*
+ * Universidade Federal de Santa Catarina.
+ * CTC - Centro Tecnologico - http://ctc.ufsc.br
+ * INE - Departamento de Informatica e Estatistica - http://inf.ufsc.br
+ */
+ 
+public abstract class Animal {
+
+
+}
+
+
---
+++ Cachorro.java
@@ -0,0 +1,9 @@
+/*
+ * Universidade Federal de Santa Catarina.
+ * CTC - Centro Tecnologico - http://ctc.ufsc.br
+ * INE - Departamento de Informatica e Estatistica - http://inf.ufsc.br
+ */
+ 
+ {
+
+}
