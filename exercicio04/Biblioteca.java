4812cc48fc3f0b6ec31fb4cd7bfb9b9ce2a3cc0f
g-saldanha <saldanha@bridge.ufsc.br>
2017-04-08 19:47:29 -0300

    construcao do codigo associativo

--- exercicio04/Biblioteca.java
+++ exercicio04/Biblioteca.java
@@ -1,12 +1,20 @@
 package exercicio04;
 
+import java.util.ArrayList;
+
 public class Biblioteca {
 
+	private ArrayList<Livro> livros;
+
 	public void incluirLivro(Livro livro){
+		//tratar equivalencia
+		this.livros.add(livro);
 
 	}
 
 	public void excluirLivro(Livro livro){
+		//tratar null
+		this.livros.remove(livro);
 
 	}
 }
--- exercicio04/Livro.java
+++ exercicio04/Livro.java
@@ -1,16 +1,96 @@
 package exercicio04;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Livro {
 	private int codigo;
 	private String titulo;
 	private int ano;
+	private Editora editora;
+	private ArrayList<Autor> autores;
 
-	public Livro(int codigo, String titulo, Editora editora, Autor autor, int numeroCapitulo, String tituloCapitulo){
-		super();
+	private ArrayList<Capitulo> capitulos = new ArrayList<>();
+
+	public Livro(int codigo, String titulo, Editora editora, Autor autor, int numeroCapitulo, String tituloCapitulo, int ano){
 		this.codigo = codigo;
+		this.titulo = titulo;
+		this.ano = ano;
+		this.editora = editora;
+		this.autores.add(autor);
+		Capitulo capitulo = new Capitulo(numeroCapitulo, tituloCapitulo);
+		this.capitulos.add(capitulo);
+
 
 	}
 
+	public int getCodigo(){
+		return this.codigo;
+	}
+
+	public String getTitulo(){
+		return this.titulo;
+	}
+
+	public int getAno(){
+		return this.ano;
+	}
+
+	public Editora getEditora(){
+		return this.editora;
+	}
+
+	public List<Autor> getAutores(){
+		return this.autores;
+	}
+
+
+	public void setCodigo(int codigo){
+		this.codigo = codigo;
+	}
+
+	public void setTitulo(String titulo){
+		this.titulo = titulo;
+	}
+
+	public void setAno(int ano){
+		this.ano = ano;
+	}
+
+	public void setEditora(Editora editora){
+		this.editora = editora;
+	}
+
+	public void incluirAutor(Autor autor){
+		//tratar igualdade
+
+	}
+
+	public void excluirAutor(Autor autor){
+		//tratar null
+
+	}
+
+	public void excluiCapitulo(String tituloCapitulo){
+		//tratar null
+	}
+
+	public void incluiCapitulo(int numero, String tituloCapitulo) {
+		//tratar igualdade
+
+
+	}
+
+	public Capitulo findCapituloByTitulo(String capitulo){
+		Capitulo capituloEncontrado = null;
+		for (Capitulo capituloProcurado : this.capitulos) {
+			if(capitulo.equals(capituloProcurado.getTitulo())){
+				capituloEncontrado = capituloProcurado;
+			}
+		}
+		return capituloEncontrado;
+
+	}
 
 
 }
